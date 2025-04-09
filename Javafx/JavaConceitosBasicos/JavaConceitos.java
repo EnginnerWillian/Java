25041372a37729158f809589278dc563f213aa04
Willian <willianflorenciocalixto4@gmail.com>
2025-04-09 18:53:43 -0300

    adsfa

---
+++ Javafx/JavaConceitosBasicos/JavaConceitos.java
@@ -0,0 +1,23 @@
+public class JavaConceitos {
+    byte b = 100;
+    short s = 10000;
+    int i = 10000;
+    long l = 10000L;
+    float f = 1000.5f;
+    double d = 102.123232
+    char a = 'a';
+    String str = "Willian";
+    boolean bool = false;
+    int[] colecaoLista = {1, 1,11, 1 1,1}
+    public void main (){
+        if(bool) {
+            System.out.print("Verdadeiro");
+        else {
+            System.out.prinnt("False")
+        }
+        System.out.print(colecaoLista)
+        }
+    }
+
+
+}
