f5d89164edd2a888091f892681040371ef105254
Willian <willianflorenciocalixto4@gmail.com>
2025-04-13 20:52:08 -0300

    add new project

---
+++ Method/Carro.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class Main {
+    public void main(String[] args){
+        Carro meuCarro1 = new Carro();
+        Carro meuCarro2 = new Carro();
+        Carro meuCarro3 = new Carro();
+    }
+
+}
+
+    class Carro{
+        public Carro(){
+                System.out.println("Carro criado");
+        }
+    }
+
---
+++ Mthods/Caneta.java
@@ -0,0 +1,17 @@
+public class Caneta {
+    public String modeo;
+    private float ponta;
+    
+    public String getModelo(){
+        return.this.modelo;
+    }
+    public void set Modelo(String m){
+       this.modelo = m;
+  }
+public float getPonta() {
+    return this.ponta;
+}
+
+public void setPonta(float p){
+    this.ponta = p; 
+}
