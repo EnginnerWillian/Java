b4c39c7cfcfe0a785903f7602d9e667164a426d0
Willian <williansuportetech@gmail.com>
2025-04-29 22:35:31 -0300

    add exercícios

--- OperadoresAritimeticos/Exercicio1/Aritimeticos.java
+++ OperadoresAritimeticos/Exercicio1/Aritimeticos.java
@@ -1,8 +1,16 @@
-public class Aritimeticos {
-    public void main(){
-        int n1 = 3;
-        int n2 = 5;
-        float m = (n1+n2)/2;
-        System.out.println("A média igual a "+m);
+import javax.swing.*;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Olá Mundo!");
+        JButton button = new JButton("Clique aqui");
+
+        button.setBounds(50, 100, 150, 40);
+
+        frame.add(button);
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
---
+++ OperadoresAritimeticos/Exercicio2/index.java
@@ -0,0 +1,11 @@
+public class index {
+    public static void{
+        int numero = 10;
+        int valor =  4 + numero--;//Pós incremento
+        System.out.println(valor)
+        int valor = 4 --numero; //Pré incremento 
+        System.out.println(numero);
+
+    }
+
+}
